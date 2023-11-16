9beb4cc437d423bfa2c5ede4f0425f6d3fb8d5cf
4aykovski <kan89kan@gmail.com>
2023-11-16 15:56:04 +0300

    js logical operators

--- front/js/learn_javascript/2/main.js
+++ front/js/learn_javascript/2/main.js
@@ -1,3 +1,19 @@
 'use strict';
-let name = prompt('Input your name', 'name');
-alert(`Your name is ${name}!`);
+let login = prompt('Введите логин:', '')
+
+if (login === 'Админ') {
+
+    let password = prompt('Введите пароль', '')
+
+    if (password === 'Я главный') {
+        alert('Здравствуйте')
+    } else if (password === '' || password === null) {
+        alert('Отменено')
+    } else {
+        alert('Неверный пароль')
+    }
+} else if (login === '' || login === null) {
+    alert('Отменено')
+} else {
+    alert('Я вас не знаю')
+}

6dff917245cea3d24b6abe1d3c0568f76de7f8c7
4aykovski <kan89kan@gmail.com>
2024-02-10 19:38:12 +0300

    react 1:54:09

--- front/learing-react/src/App.js
+++ front/learing-react/src/App.js
@@ -1,28 +1,64 @@
-import React, {useRef, useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './styles/App.css'
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
-import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/postForm";
+import PostFilter from "./components/PostFilter";
+import MyModal from "./components/UI/MyModal/MyModal";
+import {usePosts} from "./hooks/usePosts";
+import PostService from "./API/PostService";
+import Loader from "./components/UI/Loader/Loader";
+import {useFetching} from "./hooks/useFetching";
 
 
 function App() {
-    const [posts, setPosts] = useState([
-        {id: 1, title: "Javascript", body: "Description"},
-        {id: 2, title: "Golang", body: "Description"},
-        {id: 3, title: "Python", body: "Description"},
-    ])
+    const [posts, setPosts] = useState([])
+    const [filter, setFilter] = useState({sort: '', query: ''})
+    const [modal, setModal] = useState(false)
+    const sortedAndSearchedPosts = usePosts(posts, filter.post, filter.query)
+    const [fetchPosts, isPostsLoading, postError] = useFetching( async() => {
+        const posts = await PostService.getAll();
+        setPosts(posts);
+    })
+
+    useEffect(() => {
+        fetchPosts()
+    }, [])
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
+        setModal(false)
+    }
+
+    const removePost = (post) => {
+        setPosts(posts.filter(p => p.id !== post.id))
     }
 
     return (
         <div className="App">
-            <PostForm create={createPost}/>
-            <PostList
-                posts={posts}
-                title="Posts list"/>
+            <MyButton style={{marginTop: '30px'}} onClick={() => setModal(true)}>
+                Создать пользователя
+            </MyButton>
+            <MyModal visible={modal} setVisible={setModal}>
+                <PostForm create={createPost}/>
+            </MyModal>
+            <hr style={{margin: '15px 0'}}/>
+            <PostFilter
+                filter={filter}
+                setFilter={setFilter}
+            />
+            {postError &&
+            <h1>Error ${postError}</h1>}
+            {isPostsLoading
+                ? <div style={{display: 'flex', justifyContent: 'center', marginTop: '50px'}}>
+                    <Loader/>
+                </div>
+                : <PostList
+                    posts={sortedAndSearchedPosts}
+                    title="Posts list"
+                    remove={removePost}/>
+            }
+
         </div>
     );
 }

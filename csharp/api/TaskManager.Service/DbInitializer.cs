b083c101d814b5a99c91522567769227a0a6bacf
4aykovski <kan89kan@gmail.com>
2024-03-06 05:48:16 +0300

    delete

--- csharp/api/TaskManager.Service/DbInitializer.cs
+++
@@ -1,9 +0,0 @@
-namespace TaskManager.Service;
-
-public class DbInitializer
-{
-    public static void Initialize(NotesDbContext context)
-    {
-        context.Database.EnsureCreated();
-    }
-}
